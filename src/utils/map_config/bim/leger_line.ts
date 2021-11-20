20d00e9eab360bb19645ce1c27b551f8691c818a
priyayidimas <priyayidimas@upi.edu>
2021-11-20 19:00:32 +0700

    Update Leger and Version

--- src/utils/map_config/bim/leger_line.ts
+++ src/utils/map_config/bim/leger_line.ts
@@ -1,10 +1,11 @@
-const PATH = "https://services3.arcgis.com/U26uBjSD32d7xvm2/arcgis/rest/services/Sceneasd_WFL1/FeatureServer/1"
+const PATH = "https://services3.arcgis.com/kk81tlhFylVuTwCq/arcgis/rest/services/LegerMajalengka_WFL1/FeatureServer/2"
 const LAYER_ID = "bim_leger"
 const TITLE = "Data Leger Jalan (Ruas)"
 
 
 export const legerLineConfig  : __esri.FeatureLayerProperties  = {
     myType: "outsource-layer",
+    apiKey: "AAPK2021e3c0ade243ac91fc03c5cc16af553UoLz7PP3cuznJsJw2hQOU6G-m47W2PWSfHujOs9JYI-UmZOtUw7TvgwWHUSIDPI",
     url: PATH,
     title: TITLE,
     id: LAYER_ID,
--- src/utils/map_config/bim/leger_polygon.ts
+++ src/utils/map_config/bim/leger_polygon.ts
@@ -1,10 +1,11 @@
-const PATH = "https://services3.arcgis.com/U26uBjSD32d7xvm2/arcgis/rest/services/Sceneasd_WFL1/FeatureServer/2"
+const PATH = "https://services3.arcgis.com/kk81tlhFylVuTwCq/arcgis/rest/services/LegerMajalengka_WFL1/FeatureServer/3"
 const LAYER_ID = "bim_leger_polygon"
 const TITLE = "Data Leger Jalan (Polygon)"
 
 
 export const legerPolygonConfig  : __esri.FeatureLayerProperties  = {
     myType: "outsource-layer",
+    apiKey: "AAPK2021e3c0ade243ac91fc03c5cc16af553UoLz7PP3cuznJsJw2hQOU6G-m47W2PWSfHujOs9JYI-UmZOtUw7TvgwWHUSIDPI",
     url: PATH,
     title: TITLE,
     id: LAYER_ID,
